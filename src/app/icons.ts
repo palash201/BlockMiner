d1e448c9c56d4c6e5568ac3402a54ab696048f1f
Palash Keswani <palashk1003@gmail.com>
2022-06-22 22:40:10 -0400

    moved icons, edited icons.ts

--- src/app/icons.ts
+++ src/app/icons.ts
@@ -1,11 +1,11 @@
 export const icons: Map<string,string> = new Map([
-    ["stone", "../assets/SVG/stone.svg"],
-    ["pickaxe", "../assets/SVG/pickaxe.svg"],
-    ["ruby", "../assets/SVG/ruby.svg"],
-    ["clover", "../assets/SVG/clover.svg"],
-    ["enchantedBook", "../assets/SVG/enchantedBook.svg"],
-    ["forge", "../assets/SVG/forge.svg"],
-    ["quarry", "../assets/SVG/quarry.svg"],
-    ["shrine", "../assets/SVG/shrine.svg"],
-    ["experience", "../assets/SVG/experience.svg"]
+    ["stone", "../assets/stone.svg"],
+    ["pickaxe", "../assets/pickaxe.svg"],
+    ["ruby", "../assets/ruby.svg"],
+    ["clover", "../assets/clover.svg"],
+    ["enchantedBook", "../assets/enchantedBook.svg"],
+    ["forge", "../assets/forge.svg"],
+    ["quarry", "../assets/quarry.svg"],
+    ["shrine", "../assets/shrine.svg"],
+    ["experience", "../assets/experience.svg"]
 ]);
